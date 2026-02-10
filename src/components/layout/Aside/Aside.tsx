cb7374b8dc1bb97ebec74b67fd4ab5f312ebee9c
Marcelo Aug <m45marceloaugusto@gmail.com>
2026-02-09 23:12:33 -0300

    fear: criado o componente Aside

--- src/components/layout/Aside/Aside.tsx
+++ src/components/layout/Aside/Aside.tsx
@@ -3,7 +3,7 @@ import logo from "../../../../public/assets/images/Games Store/logo.svg"
 import { AsideItems } from "./AsideItems"
 export const Aside = () => {
     return (
-        <aside className="w-full fixed z-10 h-screen max-w-[237px] pt-20 bg-background flex flex-col ">
+        <aside className="w-full fixed z-10 h-screen max-w-[221px] pt-20 bg-background flex flex-col ">
             <AsideItems/>
         </aside>
     )
